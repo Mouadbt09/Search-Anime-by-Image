5d1cd6fdcb9991fc4b5863ba40e01c7a2b370528
Mouad <taibmouad19@gmail.com>
2023-11-13 21:28:35 +0100

    fix erros displaying

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const result=document.querySelector('.results')
 const imageUrl = document.querySelector('#image').value;
-function send(e){
+function send(){
     const imageUrl = document.querySelector('#image').value;
 
     if(!imageUrl) {
@@ -19,34 +19,50 @@ function send(e){
             fetch(`https://api.trace.moe/search?anilistInfo&url=${imageUrl}`)
             .then(res => {
                 if(!res.ok) {
+                  // Create the HTML content for the error messages
+                  const errorMessages = [
+                    { status: 400, cause: 'Invalid image URL / Failed to process image / OpenCV: Failed to detect and cut borders' },
+                    { status: 402, cause: 'Search quota depleted / Concurrency limit exceeded' },
+                    { status: 403, cause: 'Invalid API key' },
+                    { status: 405, cause: 'Method Not Allowed' },
+                    { status: 500, cause: 'Internal Server Error' },
+                    { status: 503, cause: 'Search queue is full / Database is not responding' },
+                    { status: 504, cause: 'Server is overloaded' }
+                  ];
+                  const matchedError=errorMessages.find(msg => msg.status === res.status)
+                  result.innerHTML=`
+                    <p>
+                        <div class="message">
+                        HTTP Status ${matchedError.status}: ${matchedError.cause} 
+                        </div>
+                    </p>
+                    `
                   throw new Error("Network response was not OK");
-                  console.log('jfhffjhgf')
-                    console.log('Error:', error);
                 }
                 return res.json();
               })
             .then(data => {
-                result.innerHTML=`
-                <div class="info">
-                  <div class='image'>
-                    <h3>You searched for: </h3>
-                    <img src="${imageUrl}" alt="" />
-                  </div>
-                  <div class='message'>
-                    <p style='margin:0px;'>
-                    <span>
-                      <img width="15" height="15" src="https://img.icons8.com/ios/50/FFFFFF/info--v1.png" alt="info--v1"/>
-                      <strong>Info:</strong>
-                    </span>
-                      The first result is usually the correct anime you are looking for 
-                    </p>
-                    <p>
-                      Similarity lower than 90% are most likely incorrect results. It's up to you to judge what is a match and what is just visually similar.
-                    </p>
-                  </div>
-                </div>
-                `
                 if(data.result){
+                  result.innerHTML=`
+                  <div class="info">
+                    <div class='image'>
+                      <h3>You searched for: </h3>
+                      <img src="${imageUrl}" alt="" />
+                    </div>
+                    <div class='message'>
+                      <p style='margin:0px;'>
+                      <span>
+                        <img width="15" height="15" src="https://img.icons8.com/ios/50/FFFFFF/info--v1.png" alt="info--v1"/>
+                        <strong>Info:</strong>
+                      </span>
+                        The first result is usually the correct anime you are looking for 
+                      </p>
+                      <p>
+                        Similarity lower than 90% are most likely incorrect results. It's up to you to judge what is a match and what is just visually similar.
+                      </p>
+                    </div>
+                  </div>
+                  `
                     // Loop through the data array
                     data.result.forEach((item) => {
                       console.log(item)
@@ -87,43 +103,12 @@ function send(e){
             })
             // catch errors
             .catch(error => {
-                // throw  error;
-                console.log('jfhffjhgf')
-                console.log('Error:', error);
-                // Create the HTML content for the error messages
-                const errorMessages = [
-                    { status: 400, cause: 'Invalid image URL / Failed to process image / OpenCV: Failed to detect and cut borders' },
-                    { status: 402, cause: 'Search quota depleted / Concurrency limit exceeded' },
-                    { status: 403, cause: 'Invalid API key' },
-                    { status: 405, cause: 'Method Not Allowed' },
-                    { status: 500, cause: 'Internal Server Error' },
-                    { status: 503, cause: 'Search queue is full / Database is not responding' },
-                    { status: 504, cause: 'Server is overloaded' }
-                ];
-
-                let errorMessageHTML = '';
-
-                if(error.status) {  
-                    const matchedError = errorMessages.find(msg => msg.status === error.status);
-
-                    errorMessageHTML = `
-                    <p>
-                        <div class="message">
-                        HTTP Status ${matchedError.status}: ${matchedError.cause} 
-                        </div>
-                    </p>
-                    `;
-
-                } else {
-                    errorMessageHTML = `
-                    <p>Unknown error occurred</p>
-                    `;
-                }
-
-                // Set the HTML content  
-                result.innerHTML = errorMessageHTML;
-
-                console.log(errorMessageHTML);
+              console.log(error)
+              // result.innerHTML=`
+              //   <div class='message'>
+              //     gg<p>${error}</p>
+              //   </div>
+              // `
             });
 
             // Function to convert seconds to time format [00:12:04]
@@ -136,4 +121,18 @@ function send(e){
           }
 
     }
+}
+function handleFileSelect(evt) {
+  const file=evt.target.files[0]
+  var imageUrl = URL.createObjectURL(file);
+
+  fetch(`https://api.trace.moe/search?anilistInfo&url=${imageUrl}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log('API response:', data);
+      // Handle the API response here
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 }
